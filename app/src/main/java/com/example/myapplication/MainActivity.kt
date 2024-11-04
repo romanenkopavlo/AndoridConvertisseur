9bf365af515bfb4e6e2174e1e505a9efd886b0e9
promanenko <pavlo.romanenko@lyceeastier.com>
2024-11-04 09:52:24 +0100

    Le debut

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,55 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.LinearLayout
+import android.widget.TextView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    private val taux:Double = 1.2
+    private var haut:Double = 0.0
+    private var bas:Double = 0.0
+    private lateinit var editTextHaut : TextView
+    private lateinit var editTextBas : TextView
+    private lateinit var buttonHaut : ImageButton
+    private lateinit var buttonBas : ImageButton
+    private lateinit var layoutLinearHaut: LinearLayout
+    private lateinit var layoutLinearBas: LinearLayout
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        editTextHaut = findViewById(R.id.editTextHaut)
+        editTextBas = findViewById(R.id.editTextBas)
+        buttonHaut = findViewById(R.id.buttonHaut)
+        buttonBas = findViewById(R.id.buttonBas)
+        layoutLinearHaut = findViewById(R.id.linearLayoutHaut)
+        layoutLinearBas = findViewById(R.id.linearLayoutBas)
+
+
+        buttonBas.setOnClickListener {
+            haut = editTextHaut.text.toString().toDouble()
+            if (haut != 0.0) {
+                bas = haut * taux
+                editTextBas.text = bas.toString()
+            } else {
+                Toast.makeText(applicationContext, "Valeur ne doit pas etre 0", Toast.LENGTH_LONG).show()
+            }
+        }
+
+        buttonHaut.setOnClickListener {
+            bas = editTextBas.text.toString().toDouble()
+            if (bas != 0.0) {
+                haut = bas * taux
+                editTextHaut.text = haut.toString()
+            } else {
+                Toast.makeText(applicationContext, "Valeur ne doit pas etre 0", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+}
